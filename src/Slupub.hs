507ab26b2ef5d70e9d12ac8acfbf78418ff8055b
Karl Pettersson <klpn1258@gmail.com>
2022-02-11 16:33:21 +0100

    Continued with Slupub support

--- src/Slupub.hs
+++ src/Slupub.hs
@@ -10,3 +10,24 @@ import           Data.List
 import           Data.Maybe
 import           GHC.Generics
 import qualified Data.Text as T
+
+data SlupubRecord = SlupubRecord {
+        publication :: SluPublication
+}
+        deriving (Show, Generic)
+
+data SluPublication = SluPublication {
+        publId :: T.Text
+        , publYear :: T.Text
+        , title :: T.Text
+}
+        deriving (Show)
+
+instance FromJSON SlupubRecord
+
+instance FromJSON SluPublication where
+        parseJSON = withObject "slupublication" $ \o -> do
+                publId <- o .: "publ_id"
+                publYear <- o .: "publ_year"
+                title <- o .: "cftitle"
+                return SluPublication{..}
